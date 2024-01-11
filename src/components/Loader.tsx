97b7e618e46c1b02b8fbfe864ddce77433d48a2d
Kumar Piyush <kr.piyush888@gmail.com>
2024-01-11 14:04:27 +0530

    updated loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,8 @@
 function Loader() {
-    return (<div className="flex items-center justify-center w-screen h-screen fixed top-0 left-0 bg-teal-50 z-50">
-             <div className = "loader"></div>
+    return (<div className="flex items-center justify-center place-items-center w-screen h-screen fixed top-0 left-0 bg-teal-50 z-50">
+             <div className = "loader">
+             </div>
+             <span>This page can take upto 2 minutes, when loading for the first time.</span>
             </div>)
 }
 
