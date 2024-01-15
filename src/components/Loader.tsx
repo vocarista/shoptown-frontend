6fffa51b5c5eda91048f2588d53e19bb6884a1d2
Kumar Piyush <kr.piyush888@gmail.com>
2024-01-16 01:44:47 +0530

    minor update to loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,8 @@
 function Loader() {
-    return (<div className="flex items-center justify-center place-items-center w-screen h-screen fixed top-0 left-0 bg-teal-50 z-50">
+    return (<div className="flex flex-col items-center justify-center place-items-center w-screen h-screen fixed top-0 left-0 bg-teal-50 z-50">
              <div className = "loader">
              </div>
-             <span className = "text-slate-200 text-xl mt-10 p-5 text-center">This page can take upto 2 minutes, when loading for the first time.</span>
+             <span className = "text-slate-400 text-xl mt-10 p-5 text-center">This page can take upto 2 minutes, when loading for the first time.</span>
             </div>)
 }
 
